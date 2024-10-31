6f513b6a0dbed967d9ee951ec23aeb3368a68965
james.bridge <james-bridge-codes@protonmail.com>
2024-10-31 23:07:36 +0000

    Add controller logic for boards

--- kanban-app-api/src/controllers/board.controller.ts
+++ kanban-app-api/src/controllers/board.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
 
 interface BoardController {
   getAllBoards: (req: Request, res: Response) => Promise<void>;
@@ -11,27 +12,195 @@ interface BoardController {
 
 const boardController: BoardController = {
   getAllBoards: async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send("Get all the boards");
+    try {
+      const user = req.user;
+
+      if (!user) {
+        res.send(401).json({ message: "Authentication failed" });
+        return;
+      }
+
+      const id = user.id;
+
+      const result = await prisma.board.findMany({
+        where: {
+          userId: id,
+          isDeleted: false,
+        },
+      });
+
+      if (!result || result.length < 1) {
+        res.status(404).json({ error: "Boards not found" });
+      }
+
+      console.log("The result is: ", result);
+
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).json({
+        message: "Error in board controller",
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   },
 
   getBoardByID: async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send("Get a board by id");
+    try {
+      const { id: boardId } = req.params;
+      const user = req.user;
+
+      if (!user) {
+        res.send(401).json({ message: "Authentication failed" });
+        return;
+      }
+
+      const userId = user.id;
+
+      const result = await prisma.board.findUnique({
+        where: {
+          id: boardId,
+          userId: userId,
+        },
+      });
+
+      if (!result) {
+        res.status(404).json({ error: "Board not found" });
+      }
+
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).json({
+        message: "Error in board controller",
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   },
 
   createBoard: async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send("Creating a new board");
+    try {
+      const user = req.user;
+
+      if (!user) {
+        res.send(401).json({ message: "Authentication failed" });
+        return;
+      }
+
+      const userId = user.id;
+      const { title } = req.body;
+
+      const result = await prisma.board.create({
+        data: {
+          title: title,
+          isDeleted: false,
+          userId: userId,
+        },
+        select: {
+          title: true,
+          columns: true,
+          id: true,
+        },
+      });
+
+      console.log("about to send new board back", result);
+
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to create new board",
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   },
 
   updateBoard: async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send("Updating a board");
+    try {
+      const user = req.user;
+
+      if (!user) {
+        res.send(401).json({ message: "Authentication failed" });
+        return;
+      }
+
+      const { id: boardId } = req.params;
+      const userId = user.id;
+      const { title } = req.body;
+
+      const result = await prisma.board.update({
+        where: {
+          id: boardId,
+          userId: userId,
+        },
+        data: {
+          title: title.trim(),
+        },
+      });
+
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to create new board",
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   },
 
   softDeteleBoard: async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send("Soft deleting a board");
+    try {
+      const user = req.user;
+
+      if (!user) {
+        res.send(401).json({ message: "Authentication failed" });
+        return;
+      }
+
+      const { id: boardId } = req.params;
+      const userId = user.id;
+
+      await prisma.board.update({
+        where: {
+          id: boardId,
+          userId: userId,
+        },
+        data: {
+          isDeleted: true,
+        },
+      });
+
+      res.status(200).json({ message: "Board successfully soft-deleted" });
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to create new board",
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   },
 
   hardDeleteBoard: async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send("Hard deleting a board");
+    try {
+      const user = req.user;
+
+      if (!user) {
+        res.send(401).json({ message: "Authentication failed" });
+        return;
+      }
+
+      const { id: boardId } = req.params;
+      const userId = user.id;
+
+      await prisma.board.delete({
+        where: {
+          userId: userId,
+          id: boardId,
+        },
+      });
+
+      res.status(204).json({ message: "Board successfully deleted" });
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to create new board",
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   },
 };
 
--- kanban-app-api/src/server.ts
+++ kanban-app-api/src/server.ts
@@ -1,10 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/ver1/auth.route";
-import boardRoutes from "./routes/ver1/board.route";
-import columnRoutes from "./routes/ver1/column.route";
-import ticketRoutes from "./routes/ver1/ticket.route";
-import taskRoutes from "./routes/ver1/task.route";
+import authRoutes from "./routes/v1/auth.route";
+import boardRoutes from "./routes/v1/board.route";
+import columnRoutes from "./routes/v1/column.route";
+import ticketRoutes from "./routes/v1/ticket.route";
+import taskRoutes from "./routes/v1/task.route";
+import { authMiddleware } from "./middleware/auth.middleware";
 
 dotenv.config();
 
@@ -12,9 +13,17 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.path);
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Express is running" });
+  if (
+    req.path === "/api/v1/auth/login" ||
+    req.path === "/api/v1/auth/register"
+  ) {
+    return next();
+  }
+
+  authMiddleware(req, res, next);
 });
 
 app.use("/api/v1/auth", authRoutes);
